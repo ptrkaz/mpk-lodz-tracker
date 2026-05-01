bb7d26eb62430dad788d425883e70c04bc04e8ba
Piotr Kazmierczak <ptrkaz@gmail.com>
2026-05-01 13:09:18 +0200

    feat(flutter): wipe RN scaffold, init Flutter project

--- __mocks__/expo-import-meta-registry.js
+++
@@ -1,7 +0,0 @@
-// Mock for expo/src/winter/ImportMetaRegistry.
-// The real module uses static `import` and is lazily required during jest teardown
-// (triggered by Object.keys(global) enumerating the __ExpoImportMetaRegistry getter).
-// At teardown isInsideTestCode=false, causing jest-runtime to throw.
-module.exports = {
-  ImportMetaRegistry: { url: '' },
-};
